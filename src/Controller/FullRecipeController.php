22fe5b129f47962b4a101227f4401b1b99373f0b
Julien Varachas <varachasjulien@gmail.com>
2023-11-26 17:09:27 +0100

    deploy

--- src/Controller/FullRecipeController.php
+++ src/Controller/FullRecipeController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use App\Entity\Recipe;
-use App\Entity\Score;
 use App\Repository\RecipeRepository;
 use App\Repository\ScoreRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
